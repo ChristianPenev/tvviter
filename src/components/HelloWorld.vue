42e777978e29d6ac968fc55ebf8947ed6685c9b4
Christian Penev <81697828+ChristianPenev@users.noreply.github.com>
2023-01-17 12:19:58 +0200

    updated drawer

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,79 +1,23 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img
-        contain
-        height="300"
-        src="@/assets/logo.svg"
-      />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" location="right">
+      <!--  -->
+    </v-navigation-drawer>
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
 
-      <div class="py-14" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://next.vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
-
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-responsive>
-  </v-container>
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
